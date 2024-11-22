022228490a5bcf26c79370380e3499fb754064a7
chuang <chuang.li94@gmail.com>
2024-11-22 09:08:36 -0500

    remove unnecessary files

--- mention-utils.js
+++
@@ -1,3 +0,0 @@
-// mention-utils.js
-export const mentionRegex = />((?:\/|\w+:\/\/)[^\s]+?|problems\b)(?=[.,;:!?]?(?=[\s\r\n]|$))/
-export const mentionRegexGlobal = new RegExp(mentionRegex.source, "g")
